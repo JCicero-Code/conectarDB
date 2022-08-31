556e2e2ab40331d07456db23bc856816324d100f
JCicero-Code <109885635+JCicero-Code@users.noreply.github.com>
2022-08-31 01:45:08 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+﻿
+(async () =>{
+    
+  const database = require('./db');
+  const Produto = require('./produto');
+  
+  try{ 
+    const result = await database.sync();
+    console.log('Conectado com sucesso ao banco de dados');
+  }catch {
+    console.log('Erro ao conectar com o banco de dados')
+  }
+    })();
